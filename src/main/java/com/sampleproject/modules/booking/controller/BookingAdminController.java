2baaca34d1364880f51826dcdfbe589356583b21
Sonjoy Tripura <onuvobtripura@gmail.com>
2026-08-13 22:10:06 +0600

    Added owner booking list, create, get controller

--- src/main/java/com/sampleproject/modules/booking/controller/BookingAdminController.java
+++ src/main/java/com/sampleproject/modules/booking/controller/BookingAdminController.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/adminBooking")
+@RequestMapping("/adminBookings")
 @RequiredArgsConstructor
 public class BookingAdminController {
 
--- src/main/java/com/sampleproject/modules/booking/controller/BookingController.java
+++ src/main/java/com/sampleproject/modules/booking/controller/BookingController.java
@@ -1,8 +1,18 @@
 package com.sampleproject.modules.booking.controller;
 
+import com.sampleproject.modules.booking.dto.BookingRequest;
+import com.sampleproject.modules.booking.dto.BookingResponse;
 import com.sampleproject.modules.booking.service.BookingService;
+import com.sampleproject.util.ApiResponse;
+import com.sampleproject.util.RequestUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bookings")
@@ -12,22 +22,44 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping("/create")
-    public void createBooking(){
-
+    public ResponseEntity<ApiResponse<BookingResponse>> createBooking(@Valid @RequestBody BookingRequest request){
+        try{
+            BookingResponse bookingResponse = this.bookingService.createBooking(request);
+            return ResponseEntity.ok(ApiResponse.success("Booking created successfully", bookingResponse));
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(ApiResponse.error(e.getMessage()));
+        }
     }
 
     @GetMapping("/list")
-    public void getBookings(){
+    public ResponseEntity<ApiResponse<?>> getBookings(RequestUtil request){
+        Page<BookingResponse> data = this.bookingService.myBookings(request);
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("objectList", data.getContent());
+        response.put("currentPage", data.getNumber());
+        response.put("totalPages", data.getTotalPages());
+        response.put("totalItems", data.getTotalElements());
+
+        response.put("sortField", request.getSortField());
+        response.put("sortDir", request.getSortDir());
+        response.put("reverseSortDir", (request.getSortDir().equals("asc") ? "desc" : "asc"));
+        return ResponseEntity.ok(ApiResponse.success("Bookings retrieve successfully", response));
 
     }
 
     @GetMapping("/get/{id}")
-    public void getBooking(@PathVariable Long id){
-
+    public ResponseEntity<ApiResponse<BookingResponse>> getBooking(@PathVariable Long id){
+        try{
+            BookingResponse bookingResponse = this.bookingService.bookingDetails(id);
+            return ResponseEntity.ok(ApiResponse.success("Booking retrieve successfully", bookingResponse));
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(ApiResponse.error(e.getMessage()));
+        }
     }
 
     @PostMapping("/pay/{id}")
-    public void assignPilot(@PathVariable Long id){
+    public void pay(@PathVariable Long id){
 
     }
 }
