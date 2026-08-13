614d053a0318181a2d713ecedfa54cba2a3df709
Sonjoy Tripura <onuvobtripura@gmail.com>
2026-08-13 23:18:06 +0600

    Added iFrame origin for h2 console

--- src/main/java/com/sampleproject/config/SecurityConfig.java
+++ src/main/java/com/sampleproject/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
